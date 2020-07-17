1ea0f8aaa4d4326d9271b887feaca90686da0000
Tom Millichamp <tom@millichamps.com>
2020-07-17 10:07:00 +0100

    add msg2 to Recording2

--- Ofsted_demo/Recording2.cs
+++ Ofsted_demo/Recording2.cs
@@ -81,6 +81,8 @@ namespace Ofsted_demo
 
             Report.Log(ReportLevel.Info, "User", "hgello", new RecordItemIndex(0));
             
+            Report.Log(ReportLevel.Info, "User", "goodbye", new RecordItemIndex(1));
+            
         }
 
 #region Image Feature Data
